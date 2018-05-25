899125ac1477c9bd3ac34feee76b1c2d5513c1d7
Rohit Kumar <rohit9075@gmail.com>
2018-05-25 15:20:43 +0530

    Initial commit

---
+++ app/src/main/java/com/rohit/jsonrecyclerview/MainActivity.java
@@ -0,0 +1,103 @@
+package com.rohit.jsonrecyclerview;
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    List<Model> mUserList=new ArrayList<>();
+
+    Model mUser;
+    RecyclerView mRecyclerView;
+    RecyclerAdapter mRecyclerAdapter;
+    private String TAG = MainActivity.class.getSimpleName();
+    ProgressDialog progressDialog;
+    String reqUrl = "http://pusuluribalaji66.000webhostapp.com/RetrofitExample/public/user";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mUserList = new ArrayList<>();
+        //new GetNews.execute();
+        new GetUsers().execute();
+    }
+    public void initViews()
+    {
+        mRecyclerView=(RecyclerView)findViewById(R.id.recyclerView);
+    }
+    public class GetUsers extends AsyncTask<Void, Void, Void> {
+
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog = new ProgressDialog(MainActivity.this);
+            progressDialog.setMessage("please wait");
+            progressDialog.setCancelable(false);
+            progressDialog.show();
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            if (progressDialog.isShowing())
+                progressDialog.dismiss();
+            mRecyclerAdapter = new RecyclerAdapter(getApplicationContext(), mUserList);
+            mRecyclerView.setAdapter(mRecyclerAdapter);
+
+        }
+        @Override
+        protected Void doInBackground(Void... voids) {
+            HttpHandler httpHandler = new HttpHandler();
+            String jsonString = httpHandler.makeServiceCall(reqUrl);
+            if (jsonString != null) {
+                try {
+                    JSONObject jsonObject = new JSONObject(jsonString);
+                    JSONArray article = jsonObject.getJSONArray("users");
+
+                    for (int i = 0; i < article.length(); i++) {
+                        JSONObject jsonObject1 = article.optJSONObject(i);
+                        Model mUser=new Model();
+                        mUser.setId(jsonObject1.getString("id"));
+                        mUser.setEmail(jsonObject1.getString("email"));
+                        mUser.setName(jsonObject1.getString("name"));
+                        mUser.setGender(jsonObject1.getString("gender"));
+                        mUserList.add(mUser);
+                    }
+                } catch (JSONException e) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(), "parser error", Toast.LENGTH_LONG).show();
+                        }
+                    });
+                    e.printStackTrace();
+                }
+            } else {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(), "can't get json file", Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+            return null;
+        }
+    }
+
+}
---
+++ app/src/main/java/com/rohit/jsonrecyclerview/RecyclerAdapter.java
@@ -0,0 +1,61 @@
+package com.rohit.jsonrecyclerview;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyViewHolder>
+{
+    List<Model> mUserList;
+    Context context;
+
+    public RecyclerAdapter(Context context, List<Model> mUserList) {
+        this.mUserList=mUserList;
+        this.context=context;
+
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(context).inflate(R.layout.list_items,parent,false);
+        return new MyViewHolder(view);
+    }
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        Model user=mUserList.get(position);
+        holder.name.setText(user.getName());
+        holder.email.setText(user.getEmail());
+        holder.gender.setText(user.getGender());
+        holder.id.setText(user.getId());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mUserList.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        private TextView name;
+        private TextView email;
+        private TextView gender;
+        private TextView id;
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            name=(TextView)itemView.findViewById(R.id.name);
+            email=(TextView)itemView.findViewById(R.id.email);
+            gender=(TextView)itemView.findViewById(R.id.gender);
+            id=(TextView)itemView.findViewById(R.id.id);
+
+
+
+        }
+    }
+}
